616385dd8f28e796afcca6d92484cff21651f5eb
Thomas Holland <thomas@innot.de>
2008-03-01 10:51:13 +0000

    Added some initial console support
    svn path=/avr_eclipse_plugin_2.0/trunk/; revision=337

--- de.innot.avreclipse.core/src/de/innot/avreclipse/AVRPluginActivator.java
+++ de.innot.avreclipse.core/src/de/innot/avreclipse/AVRPluginActivator.java
@@ -14,6 +14,10 @@ import org.eclipse.core.runtime.ILog;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -26,6 +30,7 @@ public class AVRPluginActivator extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "de.innot.avreclipse.core";
+	public static final String DEFAULT_CONSOLE = "AVR Eclipse Plugin Log";
 
 	// The shared instance
 	private static AVRPluginActivator plugin;
@@ -45,10 +50,10 @@ public class AVRPluginActivator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-		
+
 		// initialize the System path cache.
 		// Especially on posix platforms it may take a while to find
-		// the paths. SystemPathHelper.initSystemPaths() starts a 
+		// the paths. SystemPathHelper.initSystemPaths() starts a
 		// background job to do the actual initialization.
 		SystemPathHelper.initSystemPaths();
 	}
@@ -84,9 +89,11 @@ public class AVRPluginActivator extends AbstractUIPlugin {
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
-	
+
 	/**
-	 * Log the given status and print it to the err Stream if debugging is enabled.
+	 * Log the given status and print it to the err Stream if debugging is
+	 * enabled.
+	 * 
 	 * @param status
 	 */
 	public void log(IStatus status) {
@@ -95,15 +102,60 @@ public class AVRPluginActivator extends AbstractUIPlugin {
 			log.log(status);
 		}
 		if (isDebugging()) {
-			System.err.print(PLUGIN_ID+": " + status.getMessage());
+			System.err.print(PLUGIN_ID + ": " + status.getMessage());
 			if (status.getCode() != 0) {
-				System.err.print("("+status.getCode()+")");
+				System.err.print("(" + status.getCode() + ")");
 			}
 			System.out.println("");
 			if (status.getException() != null) {
 				status.getException().printStackTrace();
 			}
 		}
-		
+	}
+
+	/**
+	 * Gets the default console for this plugin.
+	 * <p>
+	 * This console is used for logging internal information, like the output of
+	 * the external tools called internally (not by user interaction). This
+	 * Console is only for background information and debugging and should never
+	 * be actively brought to the front.
+	 * </p>
+	 * 
+	 * @return The default <code>MessageConsole</code>
+	 */
+	public MessageConsole getDefaultConsole() {
+		return getConsole(DEFAULT_CONSOLE);
+	}
+
+	/**
+	 * Gets the console with the given name.
+	 * <p>
+	 * This is a convenience method to get a console with the given name.
+	 * </p>
+	 * <p>
+	 * If the console already exists, a reference is returned, otherwise a new
+	 * <code>MessageConsole</code> with the given name is created, added to
+	 * the ConsoleManager, and returned.
+	 * </p>
+	 * 
+	 * @param name
+	 *            The name of the Console
+	 * @return A <code>MessageConsole</code> with the given name
+	 */
+	public MessageConsole getConsole(String name) {
+		// Get a list of all known Consoles from the Global Console Manager and
+		// see if a Console with the given name already exists.
+		IConsoleManager conman = ConsolePlugin.getDefault().getConsoleManager();
+		IConsole[] allconsoles = conman.getConsoles();
+		for (IConsole console : allconsoles) {
+			if (console.getName().equals(name)) {
+				return (MessageConsole) console;
+			}
+		}
+		// Console not found - create a new one
+		MessageConsole newconsole = new MessageConsole(name, null);
+		conman.addConsoles(new IConsole[] { newconsole });
+		return newconsole;
 	}
 }
