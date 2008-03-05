3789fa784af7676df29979de28f0361e82e8b59b
Thomas Holland <thomas@innot.de>
2008-03-05 07:56:11 +0000

    Update JUnit Tests
    svn path=/avr_eclipse_plugin_2.0/trunk/; revision=345

--- de.innot.avreclipse-tests/src/de/innot/avreclipse/core/toolinfo/TestExternalCommandLauncher.java
+++ de.innot.avreclipse-tests/src/de/innot/avreclipse/core/toolinfo/TestExternalCommandLauncher.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 import org.junit.Test;
 
+import de.innot.avreclipse.core.toolinfo.ICommandOutputListener.StreamSource;
+
 public class TestExternalCommandLauncher {
 
 	@Test
@@ -26,16 +28,21 @@ public class TestExternalCommandLauncher {
 		assertEquals("Wrong stdout value", "test1", stdout.get(0));
 
 		// Test 2: the LogEventListener
+		final List<StreamSource> sources = new ArrayList<StreamSource>();
+		final List<String> lines = new ArrayList<String>(); 
+		arguments = new ArrayList<String>(1);
 		arguments.add("test2");
 		testlauncher = new ExternalCommandLauncher(command, arguments);
 		testlauncher.setCommandOutputListener(new ICommandOutputListener() {
 			public synchronized void handleLine(String line, StreamSource source) {
-				assertEquals("Wrong Stream", StreamSource.STDOUT, source);
-				assertEquals("Wrong line", "test2", line);
+				sources.add(source);
+				lines.add(line);
 			}
 		});
 		result = testlauncher.launch();
 		assertEquals("Launcher return code", 0, result);
+		assertEquals("Wrong Stream", StreamSource.STDOUT, sources.get(0));
+		assertEquals("Wrong line", "test2", lines.get(0));
 
 		// Test 3: a canceled launch
 
