a180dbdda6028e76cd8868badc817996f6c06fd6
Thomas Holland <thomas@innot.de>
2008-04-15 18:59:09 +0000

    Changed the visual of the avrdude command preview box to a ExpandableComposite and included code to save the layout of the page on exit.
    svn path=/avr_eclipse_plugin_2.0/trunk/; revision=401

--- de.innot.avreclipse.core/src/de/innot/avreclipse/ui/propertypages/PageAVRDude.java
+++ de.innot.avreclipse.core/src/de/innot/avreclipse/ui/propertypages/PageAVRDude.java
@@ -19,14 +19,23 @@ import java.util.List;
 
 import org.eclipse.cdt.managedbuilder.core.IConfiguration;
 import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.IScopeContext;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.forms.events.ExpansionAdapter;
+import org.eclipse.ui.forms.events.ExpansionEvent;
+import org.eclipse.ui.forms.widgets.ExpandableComposite;
+import org.osgi.service.prefs.BackingStoreException;
 
+import de.innot.avreclipse.AVRPlugin;
 import de.innot.avreclipse.core.properties.AVRDudeProperties;
 import de.innot.avreclipse.core.properties.AVRProjectProperties;
 
@@ -42,6 +51,11 @@ import de.innot.avreclipse.core.properties.AVRProjectProperties;
  * modified in the current target properties. The preview will then be updated
  * accordingly.
  * </p>
+ * <p>
+ * As a convenience for the user, the state of the preview section (expanded /
+ * collapsed) and the size ratio between tabs and preview (based on a
+ * <code>SashForm</code>) is saved when the page is disposed and restored
+ * when the page is created.
  * </p>
  * 
  * @author Thomas Holland
@@ -52,6 +66,31 @@ public class PageAVRDude extends AbstractAVRPage {
 
 	/** The preview <code>Text</code> Control */
 	private Text fPreviewText;
+
+	/** The <code>SashForm</code> for this page */
+	private SashForm fSashForm;
+
+	/** The Preview Composite */
+	private ExpandableComposite fPreviewCompo;
+
+	// The weights for the sash form, depending on the state of the
+	// ExpandableComposite for the preview text control.
+	// These values have been determined empirically and look good on my
+	// system.
+	private final int[][] fSashWeights = new int[][] { { 80, 20 }, { 95, 5 } };
+	private final static int EXPANDED = 0;
+	private final static int COLLAPSED = 1;
+
+	private final static String TEXT_PREVIEW = "AVRDude command line preview";
+
+	private static final String CLASSNAME = "avrdudepropertypage";
+	private static final String QUALIFIER = AVRPlugin.PLUGIN_ID + "/" + CLASSNAME;
+
+	private static final String KEY_ISEXPANDED = "previewexpanded";
+	private static final String KEY_WEIGHT_TOP_EXP = "weight_top_expanded";
+	private static final String KEY_WEIGHT_PREVIEW_EXP = "weight_preview_expanded";
+	private static final String KEY_WEIGHT_TOP_COLL = "weight_top_collapsed";
+	private static final String KEY_WEIGHT_PREVIEW_COLL = "weight_preview_collapsed";
 
 	/**
 	 * Set up the page for avrdude tabs.
@@ -64,33 +103,60 @@ public class PageAVRDude extends AbstractAVRPage {
 	public void createWidgets(Composite c) {
 
 		// Create the sash form
-		SashForm sashform = new SashForm(c, SWT.VERTICAL);
-		sashform.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		sashform.setLayout(new GridLayout(1, false));
+		fSashForm = new SashForm(c, SWT.VERTICAL);
+		fSashForm.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		fSashForm.setLayout(new GridLayout(1, false));
 
 		// Let the superclass draw the tabs
-		super.createWidgets(sashform);
+		super.createWidgets(fSashForm);
 
 		// Now add the avrdude command preview box
-		// This is a group...
-		Group group = new Group(sashform, SWT.NONE);
-		group.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
-		group.setLayout(new GridLayout(1, false));
-		group.setText("avrdude command line preview");
+		// This is am ExpandableComposite...
+		fPreviewCompo = new ExpandableComposite(fSashForm, SWT.NONE);
+		fPreviewCompo.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
+		fPreviewCompo.setLayout(new GridLayout(1, false));
+		fPreviewCompo.setText(TEXT_PREVIEW);
+		fPreviewCompo.addExpansionListener(new ExpansionAdapter() {
+			@Override
+			public void expansionStateChanged(ExpansionEvent e) {
+				// Save the current weight of the sashform and
+				// load the other set
+				int[] newweights;
+				if (fPreviewCompo.isExpanded()) {
+					fSashWeights[COLLAPSED] = fSashForm.getWeights();
+					newweights = fSashWeights[EXPANDED];
+				} else {
+					fSashWeights[EXPANDED] = fSashForm.getWeights();
+					newweights = fSashWeights[COLLAPSED];
+				}
+				fSashForm.setWeights(newweights);
+			}
+		});
 
 		// ...with one Text control in it
-		fPreviewText = new Text(group, SWT.MULTI | SWT.WRAP | SWT.V_SCROLL);
+		fPreviewText = new Text(fPreviewCompo, SWT.BORDER | SWT.MULTI | SWT.WRAP | SWT.V_SCROLL);
 		fPreviewText.setEditable(false);
 		GridData gd2 = new GridData(SWT.FILL, SWT.FILL, true, true);
+		gd2.horizontalIndent = 10; // does not work!
 		fPreviewText.setLayoutData(gd2);
 
-		// Set the weights of the SashForm.
-		// The tabs get 80%, while the preview has 20%.
-		// These values have been determined empirically and look good on my
-		// system.
-		// TODO maybe the user modified setting can be saved when the page is
-		// closed.
-		sashform.setWeights(new int[] { 80, 20 });
+		// Add the Text control to the ExpandableComposite
+
+		// Read the previous page state from the preferences
+		IScopeContext scope = new InstanceScope();
+		IEclipsePreferences prefs = scope.getNode(QUALIFIER);
+		boolean isexpanded = prefs.getBoolean(KEY_ISEXPANDED, false);
+		fSashWeights[EXPANDED][0] = prefs.getInt(KEY_WEIGHT_TOP_EXP, 80);
+		fSashWeights[EXPANDED][1] = prefs.getInt(KEY_WEIGHT_PREVIEW_EXP, 20);
+		fSashWeights[COLLAPSED][0] = prefs.getInt(KEY_WEIGHT_TOP_COLL, 95);
+		fSashWeights[COLLAPSED][1] = prefs.getInt(KEY_WEIGHT_PREVIEW_COLL, 5);
+
+		int[] currentweights = isexpanded ? fSashWeights[EXPANDED] : fSashWeights[COLLAPSED];
+		// and set the SashForm
+		// weights (for the collapsed state)
+		fPreviewCompo.setClient(fPreviewText);
+		fPreviewCompo.setExpanded(isexpanded);
+		fSashForm.setWeights(currentweights);
 
 	}
 
@@ -107,8 +173,8 @@ public class PageAVRDude extends AbstractAVRPage {
 		if (fPreviewText == null) {
 			return;
 		}
-		
-		StringBuffer sb = new StringBuffer("avrdude ");
+
+		StringBuffer sb = new StringBuffer("avrdude\t");
 
 		// Get the standard AVRDude arguments as defined in the given
 		// properties.
@@ -121,14 +187,15 @@ public class PageAVRDude extends AbstractAVRPage {
 
 		sb.append("\n");
 		// Get the current configuration...
-		IConfiguration buildcfg = ManagedBuildManager
-				.getConfigurationForDescription(getResDesc().getConfiguration());
+		IConfiguration buildcfg = ManagedBuildManager.getConfigurationForDescription(getResDesc()
+		        .getConfiguration());
 
 		// ...and all action arguments for the current configuration
 		List<String> allactionargs = avrdudeprops.getActionArguments(buildcfg);
 
 		// append all actions, one per line for better readabilty
 		for (String arg : allactionargs) {
+			sb.append("\t\t\t");
 			sb.append(arg);
 			sb.append("\n");
 		}
@@ -148,6 +215,37 @@ public class PageAVRDude extends AbstractAVRPage {
 		// This page uses multiple tabs
 
 		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.cdt.ui.newui.AbstractPage#dispose()
+	 */
+	@Override
+	public void dispose() {
+		// Get the current SashForm weights
+		boolean isexpanded = fPreviewCompo.isExpanded();
+		fSashWeights[isexpanded ? EXPANDED : COLLAPSED] = fSashForm.getWeights();
+
+		// Save the current GUI state
+		IScopeContext scope = new InstanceScope();
+		IEclipsePreferences prefs = scope.getNode(QUALIFIER);
+		prefs.putBoolean(KEY_ISEXPANDED, isexpanded);
+		prefs.putInt(KEY_WEIGHT_TOP_EXP, fSashWeights[EXPANDED][0]);
+		prefs.putInt(KEY_WEIGHT_PREVIEW_EXP, fSashWeights[EXPANDED][1]);
+		prefs.putInt(KEY_WEIGHT_TOP_COLL, fSashWeights[COLLAPSED][0]);
+		prefs.putInt(KEY_WEIGHT_PREVIEW_COLL, fSashWeights[COLLAPSED][1]);
+		try {
+			prefs.flush();
+		} catch (BackingStoreException e) {
+			// Log an error that the prefs could not be written
+			IStatus status = new Status(Status.ERROR, AVRPlugin.PLUGIN_ID,
+			        "Could not write to the preferences", e);
+			AVRPlugin.getDefault().log(status);
+		}
+
+		super.dispose();
 	}
 
 }
