5fa929466fcfd349bbf4f9d98ee4cdcfc58e6378
Thomas Holland <thomas@innot.de>
2008-08-28 20:03:16 +0000

    Minor code cleanup
    svn path=/avr_eclipse_plugin_2.0/trunk/; revision=605

--- de.innot.avreclipse.core/src/de/innot/avreclipse/ui/editors/FuseFileDocumentProvider.java
+++ de.innot.avreclipse.core/src/de/innot/avreclipse/ui/editors/FuseFileDocumentProvider.java
@@ -1,6 +1,18 @@
-/**
+/*******************************************************************************
  * 
- */
+ * Copyright (c) 2008 Thomas Holland (thomas@innot.de) and others
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the GNU Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Thomas Holland - initial API and implementation
+ *     
+ * $Id$
+ *     
+ *******************************************************************************/
 package de.innot.avreclipse.ui.editors;
 
 import java.util.HashMap;
@@ -16,32 +28,38 @@ import org.eclipse.ui.texteditor.IDocumentProvider;
 import de.innot.avreclipse.core.toolinfo.fuses.ByteValues;
 
 /**
- * @author U043192
+ * @author Thomas Holland
+ * @since 2.3
  * 
  */
 public class FuseFileDocumentProvider extends ForwardingDocumentProvider {
 
-	private static FuseFileDocumentProvider					fInstance;
+	private static FuseFileDocumentProvider fInstance;
 
-	private static Map<Object, DocumentByteValuesConnector>	fConnectorsMap		= new HashMap<Object, DocumentByteValuesConnector>();
-	private static Map<Object, Integer>						fConnectorsCount	= new HashMap<Object, Integer>();
+	private static Map<Object, DocumentByteValuesConnector> fConnectorsMap = new HashMap<Object, DocumentByteValuesConnector>();
+	private static Map<Object, Integer> fConnectorsCount = new HashMap<Object, Integer>();
 
-	private static class InternalDocumentSetupParticipant implements IDocumentSetupParticipant {
+	private static class InternalDocumentSetupParticipant implements
+			IDocumentSetupParticipant {
 
 		public void setup(IDocument document) {
 			// nothing to setup yet
+			// In the future we could set up a partitioner here, as well as a content assist.
 		}
 	}
 
 	public static FuseFileDocumentProvider getDefault() {
+
 		if (fInstance == null) {
 			fInstance = createProvider();
 		}
 
 		return fInstance;
+
 	}
 
 	private static FuseFileDocumentProvider createProvider() {
+
 		String partitioning = "__fuses"; // don't yet know what this is for
 
 		IDocumentSetupParticipant setupparticipant = new IDocumentSetupParticipant() {
@@ -52,19 +70,24 @@ public class FuseFileDocumentProvider extends ForwardingDocumentProvider {
 
 		IDocumentProvider parentprovider = new TextFileDocumentProvider();
 
-		FuseFileDocumentProvider provider = new FuseFileDocumentProvider(partitioning,
-				setupparticipant, parentprovider);
+		FuseFileDocumentProvider provider = new FuseFileDocumentProvider(
+				partitioning, setupparticipant, parentprovider);
 
 		return provider;
+
 	}
 
 	public FuseFileDocumentProvider() {
-		super("__fuses", new InternalDocumentSetupParticipant(), new TextFileDocumentProvider());
+		super("__fuses", new InternalDocumentSetupParticipant(),
+				new TextFileDocumentProvider());
+
 	}
 
 	private FuseFileDocumentProvider(String partitioning,
-			IDocumentSetupParticipant documentSetupParticipant, IDocumentProvider parentProvider) {
+			IDocumentSetupParticipant documentSetupParticipant,
+			IDocumentProvider parentProvider) {
 		super(partitioning, documentSetupParticipant, parentProvider);
+
 	}
 
 	/*
@@ -74,11 +97,13 @@ public class FuseFileDocumentProvider extends ForwardingDocumentProvider {
 	 */
 	@Override
 	public void connect(Object element) throws CoreException {
+
 		super.connect(element);
 
 		DocumentByteValuesConnector connector = fConnectorsMap.get(element);
 		if (connector == null) {
-			connector = new DocumentByteValuesConnector(this, element, getDocument(element));
+			connector = new DocumentByteValuesConnector(this, element,
+					getDocument(element));
 			fConnectorsMap.put(element, connector);
 			fConnectorsCount.put(element, 1);
 		} else {
@@ -87,6 +112,7 @@ public class FuseFileDocumentProvider extends ForwardingDocumentProvider {
 			int usecount = fConnectorsCount.get(element);
 			fConnectorsCount.put(element, usecount + 1);
 		}
+
 	}
 
 	/*
@@ -96,11 +122,13 @@ public class FuseFileDocumentProvider extends ForwardingDocumentProvider {
 	 */
 	@Override
 	public void disconnect(Object element) {
+
 		// TODO Auto-generated method stub
 		super.disconnect(element);
 		Integer usecount = fConnectorsCount.get(element);
 		if (usecount == null) {
 			// the element was never connected
+
 			return;
 		} else {
 			--usecount;
@@ -115,6 +143,7 @@ public class FuseFileDocumentProvider extends ForwardingDocumentProvider {
 				fConnectorsCount.put(element, usecount);
 			}
 		}
+
 	}
 
 	public ByteValues getByteValues(Object element) throws CoreException {
@@ -122,17 +151,23 @@ public class FuseFileDocumentProvider extends ForwardingDocumentProvider {
 		DocumentByteValuesConnector connector = fConnectorsMap.get(element);
 		if (connector == null) {
 			// probably not connected yet
+
 			return null;
 		}
+
 		return connector.getByteValues();
+
 	}
 
 	public void setByteValues(Object element, ByteValues newvalues) {
+
 		DocumentByteValuesConnector connector = fConnectorsMap.get(element);
 		if (connector == null) {
 			// probably not connected yet
+
 			return;
 		}
 		connector.writeByteValues(newvalues);
+
 	}
 }
