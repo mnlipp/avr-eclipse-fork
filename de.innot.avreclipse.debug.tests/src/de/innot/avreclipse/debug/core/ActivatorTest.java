27938a92034adf1c37f32ca0da0ae297bcc455ff
Thomas Holland <thomas@innot.de>
2008-12-16 15:06:30 +0000

    minor refactoring fix
    svn path=/trunk/; revision=710

--- de.innot.avreclipse.debug.tests/src/de/innot/avreclipse/debug/core/ActivatorTest.java
+++ de.innot.avreclipse.debug.tests/src/de/innot/avreclipse/debug/core/ActivatorTest.java
@@ -36,40 +36,40 @@ import org.junit.Test;
 public class ActivatorTest {
 
 	/**
-	 * Test method for {@link de.innot.avreclipse.debug.core.Activator#getDefault()}.
+	 * Test method for {@link de.innot.avreclipse.debug.core.AVRDebugPlugin#getDefault()}.
 	 */
 	@Test
 	public void testGetDefault() {
-		Activator plugin = Activator.getDefault();
+		AVRDebugPlugin plugin = AVRDebugPlugin.getDefault();
 		assertNotNull(plugin);
 	}
 
 	/**
 	 * Test method for
-	 * {@link de.innot.avreclipse.debug.core.Activator#log(org.eclipse.core.runtime.IStatus)}.
+	 * {@link de.innot.avreclipse.debug.core.AVRDebugPlugin#log(org.eclipse.core.runtime.IStatus)}.
 	 */
 	@Test
 	public void testLog() {
 
 		final List<IStatus> statuslog = new ArrayList<IStatus>();
-		ILog log = Activator.getDefault().getLog();
+		ILog log = AVRDebugPlugin.getDefault().getLog();
 		log.addLogListener(new ILogListener() {
 
 			public void logging(IStatus status, String plugin) {
-				if (Activator.PLUGIN_ID.equals(plugin)) {
+				if (AVRDebugPlugin.PLUGIN_ID.equals(plugin)) {
 					statuslog.add(status);
 				}
 			}
 		});
 
 		String message = "warning logentry by unit test";
-		IStatus status = new Status(IStatus.WARNING, Activator.PLUGIN_ID, message, null);
-		Activator.log(status);
+		IStatus status = new Status(IStatus.WARNING, AVRDebugPlugin.PLUGIN_ID, message, null);
+		AVRDebugPlugin.log(status);
 		assertTrue("Warning log entry not written", statuslog.size() == 1);
 
 		message = "ok logentry by unit test";
-		status = new Status(IStatus.OK, Activator.PLUGIN_ID, message, null);
-		Activator.log(status);
+		status = new Status(IStatus.OK, AVRDebugPlugin.PLUGIN_ID, message, null);
+		AVRDebugPlugin.log(status);
 		assertTrue("OK log entry written", statuslog.size() == 1);
 
 	}
