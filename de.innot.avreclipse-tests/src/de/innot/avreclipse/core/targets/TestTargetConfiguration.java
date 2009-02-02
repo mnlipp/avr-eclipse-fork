584f1d5ddde0bc58a928827c7432149559b13508
Thomas Holland <thomas@innot.de>
2009-02-02 21:58:51 +0000

    Updated tests
    svn path=/trunk/; revision=729

---
+++ de.innot.avreclipse-tests/src/de/innot/avreclipse/core/targets/TestTargetConfiguration.java
@@ -0,0 +1,398 @@
+/*******************************************************************************
+ * 
+ * Copyright (c) 2009 Thomas Holland (thomas@innot.de) and others
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the GNU Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Thomas Holland - initial API and implementation
+ *     
+ * $Id$
+ *     
+ *******************************************************************************/
+
+package de.innot.avreclipse.core.targets;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.osgi.service.prefs.BackingStoreException;
+
+import de.innot.avreclipse.core.targets.TargetConfiguration.ITargetConfigChangeListener;
+
+/**
+ * @author Thomas Holland
+ * @since
+ * 
+ */
+public class TestTargetConfiguration {
+
+	/**
+	 * Extension of {@link TargetConfiguration} to get access to the protected constructors.
+	 * 
+	 */
+	private class MyTargetConfiguration extends TargetConfiguration {
+		protected MyTargetConfiguration(String id) {
+			super(id);
+		}
+
+		protected MyTargetConfiguration(TargetConfiguration config) {
+			super(config);
+		}
+
+	}
+
+	private TargetConfiguration	tc;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		tc = new MyTargetConfiguration("testid");
+		assertNotNull("Config is null", tc);
+		assertEquals("Wrong id", "testid", tc.getId());
+	}
+
+	/**
+	 * Test method for
+	 * {@link de.innot.avreclipse.core.targets.TargetConfiguration#TargetConfiguration(de.innot.avreclipse.core.targets.TargetConfiguration)}
+	 * .
+	 * 
+	 * @throws BackingStoreException
+	 */
+	@Test
+	public void testTargetConfigurationTargetConfiguration() throws BackingStoreException {
+
+		ITargetConfigChangeListener faillistener = new ITargetConfigChangeListener() {
+			public void attributeChange(ITargetConfiguration config, String attribute,
+					String oldvalue, String newvalue) {
+				fail("Changing an attribute of the copy caused a attributeChange event of the original");
+			}
+		};
+
+		// Change a few settings in the standard target configuration
+		tc.setName("testName");
+		tc.setDescription("testDescription");
+		tc.setMCU("testMCU");
+		tc.setFCPU(12345678);
+
+		tc.setAttribute("testattr1", "foo");
+		tc.setAttribute("testattr2", "bar");
+
+		tc.addPropertyChangeListener(faillistener);
+
+		// Now clone this target config
+		TargetConfiguration wc = new MyTargetConfiguration(tc);
+		assertNotNull("Copy is null", wc);
+
+		// Check that all changes were propagated
+		assertEquals(tc.getId(), wc.getId());
+		assertEquals(tc.getName(), wc.getName());
+		assertEquals(tc.getDescription(), wc.getDescription());
+		assertEquals(tc.getMCUId(), wc.getMCUId());
+		assertEquals(tc.getFCPU(), wc.getFCPU());
+		assertEquals("foo", wc.getAttribute("testattr1"));
+		assertEquals("bar", wc.getAttribute("testattr2"));
+
+		// Check that the listener was not copied
+		wc.setAttribute("testattr1", "baz");
+
+		// and that the copy is separate
+		assertEquals("foo", tc.getAttribute("testattr1"));
+
+		// but on safe the change should propagate to the original config
+		tc.removePropertyChangeListener(faillistener);
+		wc.doSave();
+
+		assertEquals("baz", tc.getAttribute("testattr1"));
+
+	}
+
+	/**
+	 * Test method for {@link de.innot.avreclipse.core.targets.TargetConfiguration#getId()}.
+	 */
+	@Test
+	public void testGetId() {
+		TargetConfiguration test = new MyTargetConfiguration("foobar");
+		assertNotNull(test);
+		assertEquals("foobar", test.getId());
+	}
+
+	/**
+	 * Test method for {@link de.innot.avreclipse.core.targets.TargetConfiguration#getName()}.
+	 */
+	@Test
+	public void testName() {
+		String[] testnames = new String[] { "foo", "bar", "" };
+
+		for (String name : testnames) {
+			tc.setName(name);
+
+			assertEquals(name, tc.getName());
+			assertEquals(name, tc.getAttribute(ITargetConfiguration.ATTR_NAME));
+		}
+	}
+
+	/**
+	 * Test method for {@link de.innot.avreclipse.core.targets.TargetConfiguration#getDescription()}
+	 * .
+	 */
+	@Test
+	public void testDescription() {
+		String[] testdescriptions = new String[] { "foo", "bar", "" };
+
+		for (String desc : testdescriptions) {
+			tc.setDescription(desc);
+
+			assertEquals(desc, tc.getDescription());
+			assertEquals(desc, tc.getAttribute(ITargetConfiguration.ATTR_DESCRIPTION));
+		}
+	}
+
+	/**
+	 * Test method for {@link de.innot.avreclipse.core.targets.TargetConfiguration#getMCUId()}.
+	 */
+	@Test
+	public void testMCUId() {
+		String[] testmcus = new String[] { "foo", "bar", "" };
+
+		for (String mcu : testmcus) {
+			tc.setMCU(mcu);
+
+			assertEquals(mcu, tc.getMCUId());
+			assertEquals(mcu, tc.getAttribute(ITargetConfiguration.ATTR_MCU));
+		}
+	}
+
+	/**
+	 * Test method for {@link de.innot.avreclipse.core.targets.TargetConfiguration#getFCPU()}.
+	 */
+	@Test
+	public void testFCPU() {
+		int[] testvalues = new int[] { 1234, 123, 0 };
+
+		for (int fcpu : testvalues) {
+			tc.setFCPU(fcpu);
+
+			assertEquals(fcpu, tc.getFCPU());
+			assertEquals(fcpu, Integer.parseInt(tc.getAttribute(ITargetConfiguration.ATTR_FCPU)));
+		}
+	}
+
+	/**
+	 * Test method for {@link de.innot.avreclipse.core.targets.TargetConfiguration#doSave()}.
+	 * 
+	 * @throws BackingStoreException
+	 */
+	@Test
+	public void testDoSave() throws BackingStoreException {
+
+		// Make a "working copy"
+		TargetConfiguration wc = new MyTargetConfiguration(tc);
+
+		// Change a few settings in the standard target configuration
+		wc.setName("testName");
+		wc.setDescription("testDescription");
+		wc.setMCU("testMCU");
+		wc.setFCPU(12345678);
+
+		// pre-check the dirty flags
+		assertTrue(wc.isDirty());
+		assertFalse(tc.isDirty());
+
+		// save the config.
+		wc.doSave();
+
+		// check the dirty flags
+		assertFalse(wc.isDirty());
+		assertFalse(tc.isDirty());
+
+		// check that the changes were propagated to the original
+		assertEquals("testName", tc.getName());
+		assertEquals("testDescription", tc.getDescription());
+		assertEquals("testMCU", tc.getMCUId());
+		assertEquals(12345678, tc.getFCPU());
+
+		// Now reload the save config from the preference storage area with the help of the target
+		// configuration manager
+		ITargetConfiguration tc2 = TargetConfigurationManager.getDefault().getConfig(tc.getId());
+
+		// and do the same checks
+		assertEquals("testName", tc2.getName());
+		assertEquals("testDescription", tc2.getDescription());
+		assertEquals("testMCU", tc2.getMCUId());
+		assertEquals(12345678, tc2.getFCPU());
+	}
+
+	/**
+	 * Test method for {@link de.innot.avreclipse.core.targets.TargetConfiguration#setDefaults()}.
+	 */
+	@Test
+	public void testSetDefaults() {
+		String id = tc.getId();
+		// Change a few settings in the standard target configuration
+		tc.setName("testName");
+		tc.setDescription("testDescription");
+		tc.setMCU("testMCU");
+		tc.setFCPU(12345678);
+
+		// Now set the defaults and check that they have been applied
+		tc.setDefaults();
+
+		// Check that all changes were propagated
+		assertEquals(id, tc.getId());
+		assertEquals(ITargetConfiguration.DEF_NAME, tc.getName());
+		assertEquals(ITargetConfiguration.DEF_DESCRIPTION, tc.getDescription());
+		assertEquals(ITargetConfiguration.DEF_MCU, tc.getMCUId());
+		assertEquals(ITargetConfiguration.DEF_FCPU, tc.getFCPU());
+
+	}
+
+	/**
+	 * Test method for
+	 * {@link de.innot.avreclipse.core.targets.TargetConfiguration#getAttribute(java.lang.String)}.
+	 */
+	@Test
+	public void testAttribute() {
+		assertFalse(tc.isDirty());
+
+		tc.setAttribute("foo", "bar");
+
+		assertTrue(tc.isDirty());
+		assertEquals("bar", tc.getAttribute("foo"));
+
+		// Test failures
+		try {
+			tc.setAttribute(null, "bar");
+			fail("setAttribute(null, xxx) did not throw assertion error");
+		} catch (Exception e) {
+			// Exception expected
+		}
+		try {
+			tc.setAttribute("foo", null);
+			fail("setAttribute(xxx, null) did not throw assertion error");
+		} catch (Exception e) {
+			// Exception expected
+		}
+	}
+
+	/**
+	 * Test method for {@link de.innot.avreclipse.core.targets.TargetConfiguration#getAttributes()}.
+	 */
+	@Test
+	public void testGetAttributes() {
+		// Add one more custom attribute
+		tc.setAttribute("foo", "bar");
+
+		// now get the list of all attributes and check that the standard attributes + the custom
+		// attr are in the list
+		Map<String, String> attrmap = tc.getAttributes();
+
+		assertNotNull(attrmap);
+		assertTrue(attrmap.size() > 0);
+
+		assertTrue(attrmap.containsKey(ITargetConfiguration.ATTR_NAME));
+		assertTrue(attrmap.containsKey(ITargetConfiguration.ATTR_DESCRIPTION));
+		assertTrue(attrmap.containsKey(ITargetConfiguration.ATTR_MCU));
+		assertTrue(attrmap.containsKey(ITargetConfiguration.ATTR_FCPU));
+		assertTrue(attrmap.containsKey("foo"));
+
+		assertFalse(attrmap.containsKey(null));
+
+		// Test failures
+		try {
+			tc.getAttribute(null);
+			fail("getAttribute(null) did not throw assertion error");
+		} catch (Exception e) {
+			// Exception expected
+		}
+
+	}
+
+	/**
+	 * Test method for {@link de.innot.avreclipse.core.targets.TargetConfiguration#dispose()}.
+	 */
+	@Test
+	public void testDispose() {
+		ITargetConfigChangeListener faillistener = new ITargetConfigChangeListener() {
+			public void attributeChange(ITargetConfiguration config, String attribute,
+					String oldvalue, String newvalue) {
+				fail("Change event generated after dispose()");
+			}
+		};
+
+		tc.addPropertyChangeListener(faillistener);
+
+		tc.dispose();
+		// the next line will cause a change event, but the listener should be disposed
+		tc.setName("foobar");
+	}
+
+	private String	fAttribute;
+	private String	fOldValue;
+	private String	fNewValue;
+
+	/**
+	 * Test method for
+	 * {@link de.innot.avreclipse.core.targets.TargetConfiguration#addPropertyChangeListener(de.innot.avreclipse.core.targets.TargetConfiguration.ITargetConfigChangeListener)}
+	 * .
+	 */
+	@Test
+	public void testAddPropertyChangeListener() {
+		ITargetConfigChangeListener listener = new ITargetConfigChangeListener() {
+			public void attributeChange(ITargetConfiguration config, String attribute,
+					String oldvalue, String newvalue) {
+				assertSame(tc, config);
+				fAttribute = attribute;
+				fOldValue = oldvalue;
+				fNewValue = newvalue;
+			}
+		};
+
+		tc.addPropertyChangeListener(listener);
+
+		tc.setName("testname");
+		assertEquals(ITargetConfiguration.ATTR_NAME, fAttribute);
+		assertEquals("testname", fNewValue);
+
+		tc.setName("testname2");
+		assertEquals(ITargetConfiguration.ATTR_NAME, fAttribute);
+		assertEquals("testname", fOldValue);
+		assertEquals("testname2", fNewValue);
+
+	}
+
+	/**
+	 * Test method for
+	 * {@link de.innot.avreclipse.core.targets.TargetConfiguration#removePropertyChangeListener(de.innot.avreclipse.core.targets.TargetConfiguration.ITargetConfigChangeListener)}
+	 * .
+	 */
+	@Test
+	public void testRemovePropertyChangeListener() {
+		ITargetConfigChangeListener faillistener = new ITargetConfigChangeListener() {
+			public void attributeChange(ITargetConfiguration config, String attribute,
+					String oldvalue, String newvalue) {
+				fail("Change event generated after listener has been removed");
+			}
+		};
+
+		tc.addPropertyChangeListener(faillistener);
+		tc.removePropertyChangeListener(faillistener);
+
+		// the next line will cause a change event, but the listener should be removed
+		tc.setName("foobar");
+	}
+
+}
--- de.innot.avreclipse-tests/src/de/innot/avreclipse/core/toolinfo/TestAVRDude.java
+++ de.innot.avreclipse-tests/src/de/innot/avreclipse/core/toolinfo/TestAVRDude.java
@@ -3,12 +3,15 @@
  */
 package de.innot.avreclipse.core.toolinfo;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import org.eclipse.core.runtime.IPath;
@@ -17,6 +20,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import de.innot.avreclipse.core.avrdude.AVRDudeException;
+import de.innot.avreclipse.core.targets.HostInterface;
+import de.innot.avreclipse.core.targets.IProgrammer;
+import de.innot.avreclipse.core.targets.TargetInterface;
 
 /**
  * @author Thomas Holland
@@ -75,16 +81,88 @@ public class TestAVRDude {
 	 */
 	@Test
 	public void testGetProgrammersList() throws AVRDudeException {
-		Set<String> programmers = tool.getProgrammersList();
+		List<String> allids = new ArrayList<String>();
+
+		List<IProgrammer> programmers = tool.getProgrammersList();
 		assertNotNull(programmers);
 		assertTrue(programmers.size() > 5); // at least a few programmers should
 		// be in the list
-		assertTrue(programmers.contains("stk500")); // default entry for the plugin
-		assertTrue(programmers.contains("avrisp")); // last entry
-		assertTrue(programmers.contains("c2n232i")); // first entry
-		assertTrue(programmers.contains("pony-stk200")); // Bug 1984307
-		assertTrue(programmers.contains("dragon_isp")); // Bug 1984307
-		assertFalse(programmers.contains(null));
+
+		// Check the resulting list
+		for (IProgrammer type : programmers) {
+			assertNotNull("Null entry in the list of programmers", type);
+			String id = type.getId();
+			// Check all fields -- may not be empty
+			assertNotNull("Programmer without id", id);
+			assertTrue("Programmer with empty id", id.length() > 0);
+
+			assertTrue("Programmer " + id + " without description",
+					type.getDescription().length() > 0);
+			assertNotNull("Programmer " + id + " has null Host interface", type.getHostInterfaces());
+			assertNotNull("Programmer " + id + " has null Target interface", type
+					.getTargetInterfaces());
+
+			allids.add(type.getId());
+		}
+
+		// Check that the list contains a few selected entries
+		assertTrue(allids.contains("stk500")); // default entry for the plugin
+		assertTrue(allids.contains("avrisp")); // last entry
+		assertTrue(allids.contains("c2n232i")); // first entry
+		assertTrue(allids.contains("pony-stk200")); // Bug 1984307
+		assertTrue(allids.contains("dragon_isp")); // Bug 1984307
+	}
+
+	/**
+	 * Test method for
+	 * {@link de.innot.avreclipse.core.toolinfo.AVRDude#getProgrammersList(TargetInterface)} .
+	 * 
+	 * @throws AVRDudeException
+	 */
+	@Test
+	public void testGetHostTargetInterface() throws AVRDudeException {
+		final Object[][] testvalues = {//
+		{ "butterfly", 1, HostInterface.SERIAL, TargetInterface.BOOTLOADER },
+				{ "avr109", 1, HostInterface.SERIAL, TargetInterface.BOOTLOADER },
+				{ "avr910", 1, HostInterface.SERIAL, TargetInterface.BOOTLOADER },
+				{ "dragon_dw", 1, HostInterface.USB, TargetInterface.DW },
+				{ "jtag2dw", 2, HostInterface.SERIAL, TargetInterface.DW },
+				{ "dragon_pp", 1, HostInterface.USB, TargetInterface.PP },
+				{ "stk500pp", 1, HostInterface.SERIAL, TargetInterface.PP },
+				{ "stk600pp", 1, HostInterface.USB, TargetInterface.PP },
+				{ "dragon_hvsp", 1, HostInterface.USB, TargetInterface.HVSP },
+				{ "stk500hvsp", 1, HostInterface.SERIAL, TargetInterface.HVSP },
+				{ "stk600hvsp", 1, HostInterface.USB, TargetInterface.HVSP },
+				{ "dragon_jtag", 1, HostInterface.USB, TargetInterface.JTAG },
+				{ "jtag1", 2, HostInterface.SERIAL, TargetInterface.JTAG },
+				{ "jtag2", 2, HostInterface.SERIAL, TargetInterface.JTAG },
+				{ "xil", 1, HostInterface.PARALLEL, TargetInterface.JTAG },
+				{ "dragon_isp", 1, HostInterface.USB, TargetInterface.ISP },
+				{ "jtag2isp", 2, HostInterface.SERIAL, TargetInterface.ISP },
+				{ "bsd", 1, HostInterface.PARALLEL, TargetInterface.ISP },
+				{ "frank-stk200", 1, HostInterface.PARALLEL, TargetInterface.ISP },
+				{ "ponyser", 1, HostInterface.SERIAL_BB, TargetInterface.ISP },
+				{ "c2n232i", 1, HostInterface.SERIAL_BB, TargetInterface.ISP },
+				{ "usbasp", 1, HostInterface.USB, TargetInterface.ISP },
+				{ "usbtiny", 1, HostInterface.USB, TargetInterface.ISP }
+
+		};
+
+		for (Object[] testdata : testvalues) {
+			String avrdudeid = (String) testdata[0];
+			int eNumHostInterfaces = (Integer) testdata[1];
+			HostInterface eHostInterface = (HostInterface) testdata[2];
+			TargetInterface eTargetInterface = (TargetInterface) testdata[3];
+
+			IProgrammer type = AVRDude.getDefault().getProgrammer(avrdudeid);
+
+			assertEquals(avrdudeid + ": # host interfaces", eNumHostInterfaces, type
+					.getHostInterfaces().length);
+			assertEquals(avrdudeid + ": host interface", eHostInterface,
+					type.getHostInterfaces()[0]);
+			assertEquals(avrdudeid + ": target interfaces", eTargetInterface, type
+					.getTargetInterfaces());
+		}
 	}
 
 	/**
