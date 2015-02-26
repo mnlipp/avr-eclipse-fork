f04b2befe2e7a6cac51092e56cea77d55eefe0d0
Michael Lipp <mnl@mnl.de>
2015-02-26 12:37:08 +0100

    Workaround for long standing gdb bug.

--- de.innot.avreclipse.debug.core/src/de/innot/avreclipse/debug/core/service/AVRDebugServicesFactory.java
+++ de.innot.avreclipse.debug.core/src/de/innot/avreclipse/debug/core/service/AVRDebugServicesFactory.java
@@ -10,17 +10,18 @@
  *******************************************************************************/
 package de.innot.avreclipse.debug.core.service;
 
+import java.util.StringTokenizer;
+
+import org.eclipse.cdt.debug.gdbjtag.core.dsf.gdb.service.GdbJtagDebugServicesFactory;
+import org.eclipse.cdt.dsf.debug.service.IDisassembly;
 import org.eclipse.cdt.dsf.debug.service.command.ICommandControl;
-import org.eclipse.cdt.dsf.gdb.service.GdbDebugServicesFactory;
-import org.eclipse.cdt.dsf.gdb.service.command.CommandFactory_6_8;
-import org.eclipse.cdt.dsf.mi.service.command.CommandFactory;
 import org.eclipse.cdt.dsf.service.DsfSession;
 import org.eclipse.debug.core.ILaunchConfiguration;
 
 /**
  * @since 8.0
  */
-public class AVRDebugServicesFactory extends GdbDebugServicesFactory {
+public class AVRDebugServicesFactory extends GdbJtagDebugServicesFactory {
 
 
 	public AVRDebugServicesFactory(String version) {
@@ -28,15 +29,16 @@ public class AVRDebugServicesFactory extends GdbDebugServicesFactory {
 	}
 
 	protected ICommandControl createCommandControl(DsfSession session, ILaunchConfiguration config) {
-		if (GDB_7_2_VERSION.compareTo(getVersion()) <= 0) {
-			return new AVRGDBControl_7_2(session, config, new CommandFactory_6_8());
+		StringTokenizer versionParts = new StringTokenizer(getVersion(), ".");
+		int major = Integer.parseInt(versionParts.nextToken());
+		if (major >= 7) {
+			return new AVRGDBControl(session, config, new PatchedCommandFactory_6_8());
 		}
-		if (GDB_7_0_VERSION.compareTo(getVersion()) <= 0) {
-			return new AVRGDBControl_7_0(session, config, new CommandFactory_6_8());
+		int minor = Integer.parseInt(versionParts.nextToken());
+		if (major == 6 && minor >= 8) {
+			return new AVRGDBControl(session, config, new PatchedCommandFactory_6_8());
 		}
-		if (GDB_6_8_VERSION.compareTo(getVersion()) <= 0) {
-			return new AVRGDBControl(session, config, new CommandFactory_6_8());
-		}
-		return new AVRGDBControl(session, config, new CommandFactory());
+		return new AVRGDBControl(session, config, new PatchedCommandFactory());
 	}
+
 }
