ef80faa0b2c9bbfdb711e1a3a260722164430018
Michael Lipp <mnl@mnl.de>
2015-03-01 16:57:43 +0100

    Use gdb version specific final launch sequences.

--- de.innot.avreclipse.debug.core/src/de/innot/avreclipse/debug/core/service/AVRDebugServicesFactory.java
+++ de.innot.avreclipse.debug.core/src/de/innot/avreclipse/debug/core/service/AVRDebugServicesFactory.java
@@ -12,8 +12,15 @@ package de.innot.avreclipse.debug.core.service;
 
 import java.util.StringTokenizer;
 
+import org.eclipse.cdt.debug.gdbjtag.core.dsf.gdb.service.GDBJtagControl;
+import org.eclipse.cdt.debug.gdbjtag.core.dsf.gdb.service.GDBJtagControl_7_0;
+import org.eclipse.cdt.debug.gdbjtag.core.dsf.gdb.service.GDBJtagControl_7_2;
+import org.eclipse.cdt.debug.gdbjtag.core.dsf.gdb.service.GDBJtagControl_7_4;
+import org.eclipse.cdt.debug.gdbjtag.core.dsf.gdb.service.GDBJtagControl_7_7;
 import org.eclipse.cdt.debug.gdbjtag.core.dsf.gdb.service.GdbJtagDebugServicesFactory;
 import org.eclipse.cdt.dsf.debug.service.command.ICommandControl;
+import org.eclipse.cdt.dsf.gdb.service.command.CommandFactory_6_8;
+import org.eclipse.cdt.dsf.mi.service.command.CommandFactory;
 import org.eclipse.cdt.dsf.service.DsfSession;
 import org.eclipse.debug.core.ILaunchConfiguration;
 
@@ -30,14 +37,20 @@ public class AVRDebugServicesFactory extends GdbJtagDebugServicesFactory {
 	protected ICommandControl createCommandControl(DsfSession session, ILaunchConfiguration config) {
 		StringTokenizer versionParts = new StringTokenizer(getVersion(), ".");
 		int major = Integer.parseInt(versionParts.nextToken());
-		if (major >= 7) {
-			return new AVRGDBControl(session, config, new PatchedCommandFactory_6_8());
-		}
 		int minor = Integer.parseInt(versionParts.nextToken());
-		if (major == 6 && minor >= 8) {
-			return new AVRGDBControl(session, config, new PatchedCommandFactory_6_8());
+		if (major == 7 && minor >= 7) {
+			return new GDBJtagControl_7_7(session, config, new PatchedCommandFactory_6_8());
 		}
-		return new AVRGDBControl(session, config, new PatchedCommandFactory());
+		if (major == 7 && minor >= 4) {
+			return new GDBJtagControl_7_4(session, config, new PatchedCommandFactory_6_8());
+		}
+		if (major == 7 && minor >= 2) {
+			return new GDBJtagControl_7_2(session, config, new PatchedCommandFactory_6_8());
+		}
+		if (major == 7) {
+			return new GDBJtagControl_7_0(session, config, new PatchedCommandFactory_6_8());
+		}
+		return new GDBJtagControl(session, config, new PatchedCommandFactory());
 	}
 
 }
